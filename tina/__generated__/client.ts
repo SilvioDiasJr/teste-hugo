372ba4b2ac326eeacf5ceb0cbfc9a24110c5f910
Silvio Dias <silviodiasjr@outlook.com>
2023-08-31 16:51:22 -0300

    Delete tina/__generated__ directory

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '7a6c7864617e9083bb1837e58fd40e2b09726fca', queries });
-export default client;
-  
